7a8699792807cae87755d1b01b2b35f0ebe0cce2
Leyla Bonilla <leyla.bonilla@bairesdev.com>
2022-01-03 15:35:39 -0500

    added stripe payments in sign up functionality

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable, :confirmable, 
+  # for confirmable you have to configure the send email
+  devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_one :payment
+  # when sign up i will create user and payment table so i need the payment attributes too
+  accepts_nested_attributes_for :payment
 end
