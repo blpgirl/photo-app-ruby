08a8a3bbfc6cef3dfa9414360862d46260c82d8e
Leyla Bonilla <leyla.bonilla@bairesdev.com>
2022-01-03 09:46:07 -0500

    add devise and bootstrap

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,7 @@
 class WelcomeController < ApplicationController
+  # to not require authentication here, declare for all app in ApplicationController
+  skip_before_action :authenticate_user!, only: [:index]
+
   def index
   end
 end
